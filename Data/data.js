7c91d18226d17513af13331d4535554b13a95e8a
collinsbj <collinsbj@me.com>
2017-12-20 10:43:50 -0700

    updated data

--- Data/data.js
+++ Data/data.js
@@ -1,7 +1,21 @@
 var clothes = [
   {
-    id: 1,
-    name: "James"
+    type: "top",
+    event: "wedding",
+    eventType: "formal",
+    itemType: "tuxedo",
+    itemName: "Sherman Trim Fit Flocked Velvet Dinner Jacket",
+    description:
+      "This handsome one-button jacket crafted in soft flocked velvet sharpens your appearance with utter sophistication and unmistakable style.",
+    price: 645,
+    sizeRange: "2S-73XL",
+    color: "Navy",
+    storeName: "Nordstrom",
+    brand: "TED BAKER LONDON",
+    website:
+      "https://shop.nordstrom.com/s/ted-baker-london-sherman-trim-fit-flocked-velvet-dinner-jacket/4752612",
+    img:
+      "https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/18/_102197118.jpg?crop=pad&pad_color=FFF&format=jpeg&w=391&h=600&dpr=2&quality=60"
   }
 ];
 
