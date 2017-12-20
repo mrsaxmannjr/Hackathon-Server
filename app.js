66f7aebe74b324b37b1c22b80834e10799598fd9
mrsaxmannjr <mrsaxmannjr@gmail.com>
2017-12-19 19:44:48 -0700

    setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const data = require("./Data/data");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+  response.json(response);
+});
+
+app.post("/", (request, response) => {
+  response.json(request.body);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on port 3000');
+});
